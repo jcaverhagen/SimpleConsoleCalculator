165289ef2c7f6db8b265ebf55a017a38c1ab3179
Jan Verhagen <verhagen.jca@gmail.com>
2015-05-19 19:42:45 +0200

    Added an Calculation class

---
+++ Calculation.h
@@ -0,0 +1,19 @@
+class Calculation
+{
+    public :
+    int plusCalc(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    int minusCalc(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    int divideCalc(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    int multiplyCalc(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+};
---
+++ include/calculator.h
@@ -0,0 +1,14 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+class calculator
+{
+    public:
+        calculator();
+        virtual ~calculator();
+    protected:
+    private:
+};
+
+#endif // CALCULATOR_H
